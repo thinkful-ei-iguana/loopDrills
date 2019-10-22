16773717889ae7c03b37efe311b8535c1beb4b19
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-22 12:42:44 -0700

    Working on reduce function

--- highorderfunction.js
+++ highorderfunction.js
@@ -1,53 +1,57 @@
 'use strict'
-function repeat(fn, n){
-    for(i = 0; i < n; i++){
-        fn();
-}
-}
-function hello(){
-    console.log('Hello world');
-}
-function goodbye(){
-    console.log('Goodbye world');
-}
+// function repeat(fn, n){
+//     for(i = 0; i < n; i++){
+//         fn();
+// }
+// }
+// function hello(){
+//     console.log('Hello world');
+// }
+// function goodbye(){
+//     console.log('Goodbye world');
+// }
 
 
-function filter(arr, fn) {
-    const newArray = [];
-    for(i= 0; i < arr.length; i++){
-        if(fn(arr[i]) === true){
-            newArray.push(arr[i]);
-        }
-    }
- return newArray;
-}
+// function filter(arr, fn) {
+//     const newArray = [];
+//     for(i= 0; i < arr.length; i++){
+//         if(fn(arr[i]) === true){
+//             newArray.push(arr[i]);
+//         }
+//     }
+//  return newArray;
+// }
 
-// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
-// First we setup an array of strings we plan to filter:
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
-// We use your `filter` function here, capturing a new array into `filteredNames`
-// comprised of names that only begin with 'R'
-const filteredNames = filter(myNames, function(name) {
-    // This is known as a "predicate function" - it's a function that 
-    // only returns a boolean
-    return name[0] === 'R';
-});
+// // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// // First we setup an array of strings we plan to filter:
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// // We use your `filter` function here, capturing a new array into `filteredNames`
+// // comprised of names that only begin with 'R'
+// const filteredNames = filter(myNames, function(name) {
+//     // This is known as a "predicate function" - it's a function that 
+//     // only returns a boolean
+//     return name[0] === 'R';
+// });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
-// <---- DO NOT EDIT BETWEEN THESE LINES
+// console.log(filteredNames) // => ['Rich', 'Ray']
+// // <---- DO NOT EDIT BETWEEN THESE LINES
 
 
-function hazardWarningCreator(typeOfWarning){
-    let warningCounter = 0;
-    return function(location){
-        warningCounter++;
-        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
-        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
-    }
-    }
-    const rocksWarning = hazardWarningCreator('Rocks on the Road');
-    const landSlide = hazardWarningCreator("Landslide");
-    const tornadoes = hazardWarningCreator("Huge Tornadoes");
-    rocksWarning("Main Street and Grim Street");
-    landSlide("Dirt Road and Soggy Canyon");
-    tornadoes("Windy Street and Barebones Road");
+// function hazardWarningCreator(typeOfWarning){
+//     let warningCounter = 0;
+//     return function(location){
+//         warningCounter++;
+//         console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+//         console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+//     }
+//     }
+//     const rocksWarning = hazardWarningCreator('Rocks on the Road');
+//     const landSlide = hazardWarningCreator("Landslide");
+//     const tornadoes = hazardWarningCreator("Huge Tornadoes");
+//     rocksWarning("Main Street and Grim Street");
+//     landSlide("Dirt Road and Soggy Canyon");
+//     tornadoes("Windy Street and Barebones Road");
+
+    let turtleMoves = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+    let newMoves = turtleMoves.filter(function(x){ return x > -1;});
+    console.log(newMoves);
